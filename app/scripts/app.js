704e32c034dab8d11fd7106cac1f110511a0429b
Hendrik Putzek <hendrik.putzek@dkd.de>
2015-04-20 09:15:09 +0200

    remove custom tablet element...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 (function (document) {
   'use strict';
 
+
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
@@ -12,15 +13,21 @@
   app.addEventListener('template-bound', function() {
 		$('#programChange').on('core-activate',function(e,detail){
 			activeProgram = e.currentTarget.selectedIndex_ +1;
+			loadStep();
 		});
 	  updateStep(activeProgram);
 	  handlePrevious();
 	  handleNext();
+	  leds = $('tablet-glass');
   });
 
 	var programs = createArray(4,16,16);
 	var activeProgram = 1;
 	var activeStep = 1;
+	var leds = 0;
+	var output = "";
+	saveStep();
+
 
 
 	function randomize(){
@@ -44,6 +51,7 @@
 
 	function handlePrevious(){
 		$('#previous').click(function(){
+			saveStep();
 			if(activeStep > 1){
 				activeStep --;
 			}
@@ -51,20 +59,46 @@
 				activeStep = 16;
 			}
 			console.log('prev');
+			loadStep();
 			updateStep(activeStep);
 		});
 	}
 	function handleNext(){
 		$('#next').click(function(){
+			saveStep();
 			if(activeStep < 16){
 				activeStep ++;
 			}
 			else {
 				activeStep = 1;
 			}
+			loadStep();
 			updateStep(activeStep);
 		});
 	}
+
+	function saveStep() {
+		for (var i = 0; i < 16; i++) {
+			programs[activeProgram-1][activeStep-1][i]= $(leds[i]).attr('lightness');
+		}
+	}
+
+	function loadStep() {
+		for (var i = 0; i < 16; i++) {
+			if(programs[activeProgram-1][activeStep-1][i] == undefined){
+				$(leds[i]).attr('lightness',0);
+			}
+			else {
+				$(leds[i]).attr('lightness',programs[activeProgram-1][activeStep-1][i]);
+			}
+
+		}
+	}
+
+	function showOutput() {
+		// generate Output here
+	}
+
 
 	function updateStep(stepNumber){
 		$('#step').attr('value',100/16*stepNumber);
